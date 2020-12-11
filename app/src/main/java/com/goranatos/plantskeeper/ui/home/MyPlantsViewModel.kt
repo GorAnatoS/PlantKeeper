2b09cb55421336b74cf98a4ef8da95f623b6398a
GorAnatoS <qsufff@gmail.com>
2020-12-11 20:04:02 +0300

    only needed in MyPlantsViewModel.kt

--- app/src/main/java/com/goranatos/plantskeeper/ui/home/MyPlantsViewModel.kt
+++ app/src/main/java/com/goranatos/plantskeeper/ui/home/MyPlantsViewModel.kt
@@ -9,12 +9,7 @@ import kotlinx.coroutines.*
 
 class MyPlantsViewModel(private val repository: PlantsRepository) :
     ViewModel() {
-
-
-    private val viewModelJob = Job()
-
-    val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
-
+    
     lateinit var allPlants: LiveData<List<Plant>>
 
     private val _navigateToThePlant = MutableLiveData<Boolean?>()
@@ -35,29 +30,4 @@ class MyPlantsViewModel(private val repository: PlantsRepository) :
             allPlants = repository.getAllMyPlants().asLiveData()
         }
     }
-
-    suspend fun updatePlant(plant: Plant) {
-        withContext(Dispatchers.IO) {
-            repository.updatePlant(plant)
-        }
-    }
-
-    suspend fun deletePlant(plant: Plant) {
-        withContext(Dispatchers.IO) {
-            repository.deletePlant(plant)
-        }
-    }
-
-    suspend fun getPlant(id: Int): LiveData<Plant> {
-        return withContext(Dispatchers.IO) {
-            repository.getPlant(id)
-        }
-    }
-
-
-    override fun onCleared() {
-        super.onCleared()
-        viewModelJob.cancel()
-    }
-
 }
